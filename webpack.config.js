8194f8f342241484e1717d45cf09fd3bb3192725
Murillo Mendes <murillomendes95@hotmail.com>
2021-12-16 13:14:42 -0300

    Fixed webpack problems

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
 
 module.exports = {
@@ -12,32 +12,13 @@ module.exports = {
    output: {
       path: path.resolve(__dirname, 'dist'),
       filename : "[name].bundle.js",
-      publicPath: "http://localhost:3000/"
+      // publicPath: "http://localhost:3000/"
    },
 	 plugins: [ 
       new HtmlWebpackPlugin({ 
          template: './src/index.html'
       }), 
-      new BrowserSyncPlugin(
-        // BrowserSync options
-        {
-          // browse to http://localhost:3000/ during development
-          host: 'localhost',
-          port: 3000,
-          // proxy the Webpack Dev Server endpoint
-          // (which should be serving on http://localhost:3100/)
-          // through BrowserSync
-          proxy: 'http://localhost:8080/',
-          files: ['./dist/*.html','./dist/*.css']
-        },
-        // plugin options
-        {
-          // prevent BrowserSync from reloading the page
-          // and let Webpack Dev Server take care of this
-          reload: true
-        }
-      ),
-      // new MiniCssExtractPlugin()
+      new MiniCssExtractPlugin()
    ],
    module:{
       rules:[
@@ -48,21 +29,22 @@ module.exports = {
         {
             test: /\.(s(a|c)ss)$/,
             use: [
-               "style-loader", // Injects style into DOM
+               MiniCssExtractPlugin.loader,
+               // "style-loader", // Injects style into DOM
                "css-loader",   // Turns CSS into JS
                "sass-loader"   // Turns SCSS into CSS
             ]
         },
-        {
-           test: /\.(png|svg|jpg|jpeg|gif)$/i,
-           type: 'asset/resource',
-        },
-        {
-         test: /\.(woff|woff2|eot|ttf|svg|jpg|png)$/,
-         use: {
-           loader: 'url-loader',
-         },
-        },
+      //   {
+      //      test: /\.(png|svg|jpg|jpeg|gif)$/i,
+      //      type: 'asset/resource',
+      //   },
+      //   {
+      //    test: /\.(woff|woff2|eot|ttf|svg|jpg|png)$/,
+      //    use: {
+      //      loader: 'url-loader',
+      //    },
+      //   },
       ]
    }
 };
